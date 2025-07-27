0b5560614fea377931b392d80afd6a50355e403c
CDY0520 <ekdus0520@gmail.com>
2025-07-27 20:02:20 +0900

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -13,11 +13,13 @@ import warnings
 logging.getLogger('cmdstanpy').setLevel(logging.WARNING)
 logging.getLogger('prophet').setLevel(logging.WARNING)
 warnings.filterwarnings('ignore')
+
 plt.rcParams['font.family'] = 'Malgun Gothic'
 plt.rcParams['axes.unicode_minus'] = False
 plt.rcParams['font.family'] = font_prop.get_name()
+
 font_path = './fonts/NotoSansKR-VariableFont_wght.ttf'
-font_prop = fm.FontProperties(fname=font_path)
+fontprop = fm.FontProperties(fname=font_path)
 
 
 
