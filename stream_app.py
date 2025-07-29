42d055c22b57ec27b0d21b62dfc4cb390be69ba7
CDY0520 <ekdus0520@gmail.com>
2025-07-29 15:56:03 +0900

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -379,7 +379,12 @@ y_label_hospital, y_label_community = None, None
 col1, col2, col3 = st.columns([1.1, 1.5, 1.5])
 
 with col2:
-    st.markdown(class=responsive-box, "#### 🏥 병원 감염", unsafe_allow_html=True)
+    st.markdown("""
+    <div class="responsive-box">
+        #### 🏥 병원 감염
+    </div>
+    """, unsafe_allow_html=True)
+    
     hospital_options = ["선택"] + list(hospital_file_map.keys())
     hospital_choice = st.selectbox("", hospital_options, index=0, key="hospital_select")
 
@@ -389,7 +394,12 @@ with col2:
         plot_graph(hospital_df, "병원 감염 이상치 예측", y_label_hospital, current_date)
 
 with col3:
-    st.markdown(class=responsive-box, "#### 🌐 지역사회 감염", unsafe_allow_html=True)
+    st.markdown("""
+    <div class="responsive-box">
+        #### 🌐 지역사회 감염
+    </div>
+    """, unsafe_allow_html=True)
+    
     community_options = ["선택"] + list(community_file_map.keys())
     community_choice = st.selectbox("", community_options, index=0, key="community_select")
 
@@ -399,7 +409,11 @@ with col3:
         plot_graph(community_df, "지역사회 감염 이상치 예측", y_label_community, current_date)
 
 with col1:
-    st.markdown(class="responsive-box" "#### 🔔 통합 경보", unsafe_allow_html=True)
+    st.markdown("""
+    <div class="responsive-box">
+        #### 🔔 통합 경보
+    </div>
+    """, unsafe_allow_html=True)
     st.markdown(" ")
     st.markdown(" ")
     st.markdown(" ")
